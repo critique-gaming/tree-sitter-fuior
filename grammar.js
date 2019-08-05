7aa59d2583d1499e362058ccacfe01e1d3520d09
Marius Petcu <dapetcu21@gmail.com>
2019-08-05 20:42:07 +0300

    Implement expressions

--- grammar.js
+++ grammar.js
@@ -23,15 +23,16 @@ module.exports = grammar({
 
     if_statement: $ => seq(
       'if',
-      $.expression,
+      $.condition,
       $.if_clause,
       repeat($.elseif_clause),
       optional($.else_clause),
       'end',
       $._endl,
     ),
+    condition: $ => $._expression,
     if_clause: $ => $.block,
-    elseif_clause: $ => seq('elseif', $.expression, $.block),
+    elseif_clause: $ => seq('elseif', $.condition, $.block),
     else_clause: $ => seq('else', $.block),
 
     choose_statement: $ => seq(
@@ -48,11 +49,12 @@ module.exports = grammar({
     ),
 
     stat_operation: $ => seq(
-      $.identifier,
-      $.stat_operand,
-      $.expression,
+      alias($.identifier, $.stat_lvalue),
+      $.stat_operator,
+      $.stat_rvalue,
     ),
-    stat_operand: $ => choice('+', '-', '='),
+    stat_operator: $ => choice('+', '-', '='),
+    stat_rvalue: $ => $._expression,
 
     block: $ => seq(
       $._endl,
@@ -65,9 +67,9 @@ module.exports = grammar({
       $._endl,
     ),
     command_arg: $ => choice(
-      $.string,
-      $.number,
+      $._constant,
       $.bare_word,
+      seq('{', $._expression, '}'),
     ),
     bare_word: $ => token(prec(-1, /[^\s]+/)),
 
@@ -82,14 +84,45 @@ module.exports = grammar({
     ),
     text_copy: $ => /[^\r\n]+/,
 
-    expression: $ => choice(
+    _expression: $ => choice(
       $.identifier,
+      $._constant,
+      $.unary_expression,
+      $.binary_expression,
+      $.paran_expression,
+    ),
+
+    _constant: $ => choice(
       $.number,
       $.string,
+      $.boolean,
     ),
+
+    unary_expression: $ => prec(10, choice(
+      seq('-', $._expression),
+      seq('not', $._expression),
+    )),
+
+    binary_expression: $ => choice(
+      prec.left(5, seq($._expression, '*', $._expression)),
+      prec.left(5, seq($._expression, '/', $._expression)),
+      prec.left(4, seq($._expression, '+', $._expression)),
+      prec.left(4, seq($._expression, '-', $._expression)),
+      prec.left(3, seq($._expression, '<', $._expression)),
+      prec.left(3, seq($._expression, '>', $._expression)),
+      prec.left(3, seq($._expression, '>=', $._expression)),
+      prec.left(3, seq($._expression, '<=', $._expression)),
+      prec.left(3, seq($._expression, '=', $._expression)),
+      prec.left(3, seq($._expression, '!=', $._expression)),
+      prec.left(2, seq($._expression, 'and', $._expression)),
+      prec.left(1, seq($._expression, 'or', $._expression)),
+    ),
+
+    paran_expression: $ => seq('(', $._expression, ')'),
 
     identifier: $ => /[a-z_][a-z0-9_]*/,
     number: $ => /[0-9]+(\.[0-9]+)?/,
     string: $ => /"[^"]*"/,
+    boolean: $ => choice('true', 'false'),
   }
 });
